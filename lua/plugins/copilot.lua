a3d04d8a36379f7b6a8ed02e729dd3d323d7dccf
Nacho Vazquez <nachovc1410@gmail.com>
2023-03-01 21:13:39 -0300

    fix: migrate to lazy vim starter

--- lua/plugins/copilot.lua
+++
@@ -1,9 +0,0 @@
-return {
-  {
-    "github/copilot.vim",
-    config = function()
-      vim.g.copilot_no_tab_map = true
-      vim.keymap.set("i", "<C-a>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
-    end,
-  }
-}
