82a306f2637bfb4ca969a803ad3f2bfa2fd3bdca
Nacho Vazquez <nachovc1410@gmail.com>
2023-03-04 19:12:07 -0300

    feat: adjust options and keybindings to my workflow

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,9 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+vim.keymap.set("n", "<leader>sv", "<C-w>v", { desc = "[S]plit [V]ertically" })
+vim.keymap.set("n", "<leader>sh", "<C-w>s", { desc = "[S]plit [H]orizontally" })
+vim.keymap.set("n", "<leader>se", "<C-w>=", { desc = "[S]plit [E]qually" })
+vim.keymap.set("n", "<leader>sx", ":close<CR>", { desc = "[S]plit [E]qually" })
+
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex, { desc = "Explore" })
--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -11,4 +11,24 @@ return {
       },
     },
   },
+  {
+    "echasnovski/mini.surround",
+    opts = {
+      mappings = {
+        add = "gsa",
+        delete = "gsd",
+        find = "gsf",
+        find_left = "gsF",
+        highlight = "gsh",
+        replace = "gsr",
+        update_n_lines = "gsn",
+      },
+    },
+  },
+  {
+    "rcarriga/nvim-notify",
+    opts = {
+      background_colour = "#000000",
+    },
+  },
 }
