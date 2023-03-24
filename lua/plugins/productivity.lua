b6c39d6985f29f192ed5ae0ef5fb39b533a29d69
Nacho Vazquez <nachovc1410@gmail.com>
2023-03-23 22:23:59 -0300

    feat: install neorg

---
+++ lua/plugins/productivity.lua
@@ -0,0 +1,17 @@
+return {
+  -- neorg
+  {
+    "nvim-neorg/neorg",
+    ft = "norg",
+    opts = {
+      load = {
+        ["core.defaults"] = {},
+        ["core.norg.concealer"] = {},
+        ["core.norg.completion"] = {
+          config = { engine = "nvim-cmp" },
+        },
+        ["core.integrations.nvim-cmp"] = {},
+      },
+    },
+  },
+}
