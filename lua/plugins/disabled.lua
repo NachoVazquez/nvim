3c1fe073db65bc12010dc47fe43e94d15ac5d7e8
Nacho Vazquez <nachovc1410@gmail.com>
2023-07-26 12:05:44 -0300

    feat: remove unneded configs

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -2,6 +2,7 @@ return {
   { "nvim-neo-tree/neo-tree.nvim", enabled = false },
   { "ggandor/flit.nvim", enabled = false },
   { "ggandor/leap.nvim", enabled = false },
+  { "folke/flash.nvim", enabled = false },
   {
     "rcarriga/nvim-notify",
     enabled = false,
