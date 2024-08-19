7dd3e6671e7fb99c53a5317f69ad2a9a6fc3e264
Nacho Vazquez <nacho.vazquez.dev@gmail.com>
2024-08-19 07:25:52 -0300

    fix: keys of grug

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -88,4 +88,24 @@ return {
     --   }
     -- end,
   },
+  {
+    "MagicDuck/grug-far.nvim",
+    keys = {
+      {
+        "<leader>sX",
+        function()
+          local grug = require("grug-far")
+          local ext = vim.bo.buftype == "" and vim.fn.expand("%:e")
+          grug.grug_far({
+            transient = true,
+            prefills = {
+              filesFilter = ext and ext ~= "" and "*." .. ext or nil,
+            },
+          })
+        end,
+        mode = { "n", "v" },
+        desc = "Search and Replace",
+      },
+    },
+  },
 }
