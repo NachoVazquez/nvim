9782571e2712bd5c916f3b27e984a4a07f84ea1c
Nacho Vazquez <nacho@naxo.dev>
2025-03-19 21:28:54 -0300

    fix: use preinstalled vim-daddb

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -25,44 +25,6 @@ return {
       -- LazyVim adds this configuration itself
     },
   },
-  -- Database
-  {
-    "tpope/vim-dadbod",
-    dependencies = {
-      "kristijanhusak/vim-dadbod-ui",
-      "kristijanhusak/vim-dadbod-completion",
-    },
-    opts = {
-      db_competion = function() end,
-    },
-    config = function(_, opts)
-      vim.g.db_ui_save_location = "~/work/nvim/db_ui"
-
-      vim.api.nvim_create_autocmd("FileType", {
-        pattern = {
-          "sql",
-        },
-        command = [[setlocal omnifunc=vim_dadbod_completion#omni]],
-      })
-
-      vim.api.nvim_create_autocmd("FileType", {
-        pattern = {
-          "sql",
-          "mysql",
-          "plsql",
-        },
-        callback = function()
-          vim.schedule(opts.db_completion)
-        end,
-      })
-    end,
-    keys = {
-      { "<leader>Dt", "<cmd>DBUIToggle<cr>", desc = "Toggle UI" },
-      { "<leader>Df", "<cmd>DBUIFindBuffer<cr>", desc = "Find Buffer" },
-      { "<leader>Dr", "<cmd>DBUIRenameBuffer<cr>", desc = "Rename Buffer" },
-      { "<leader>Dq", "<cmd>DBUILastQueryInfo<cr>", desc = "Last Query Info" },
-    },
-  },
   {
     "olexsmir/gopher.nvim",
     ft = "go",
