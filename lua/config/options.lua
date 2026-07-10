311fc0ebdd94f46e05fcc1083413cf8b45aa4b2b
Nacho Vazquez <nacho@naxo.dev>
2026-07-09 23:07:00 -0300

    fix!: move dadbod connection strings out of the repo

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,4 +1,7 @@
 vim.opt.clipboard = "unnamedplus" -- Sync with system clipboard
-vim.g.dbs = {
-  naxo = "postgres://postgres:wZcAReAw3iq98BV3iornowXMuQ1SAGt8@atlas-nachovazquez-postgrescluster-uekmnkwx.cluster-ccfkkagy6f0p.us-east-1.rds.amazonaws.com/atlas",
-}
+
+-- vim-dadbod connections live outside the repo (contains credentials)
+local dbs_file = vim.fn.stdpath("data") .. "/dbs.lua"
+if vim.uv.fs_stat(dbs_file) then
+  vim.g.dbs = dofile(dbs_file)
+end
